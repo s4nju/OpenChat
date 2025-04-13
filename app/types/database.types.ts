a1b4ba8c032c7504d2f78ad5a53cae7071782ab8
AjanRaj <55903526+ajanraj@users.noreply.github.com>
2025-04-13 19:22:09 +0100

    feat: update zustand to version 5.0.3 and add new @emotion/is-prop-valid dependency; enhance database types with feedback and logo structures

--- app/types/database.types.ts
+++ app/types/database.types.ts
@@ -1,5 +1,3 @@
-import { Attachment } from "@ai-sdk/ui-utils"
-
 export type Json =
   | string
   | number
@@ -14,7 +12,7 @@ export type Database = {
       chat_attachments: {
         Row: {
           chat_id: string
-          created_at: string
+          created_at: string | null
           file_name: string | null
           file_size: number | null
           file_type: string | null
@@ -24,7 +22,7 @@ export type Database = {
         }
         Insert: {
           chat_id: string
-          created_at?: string
+          created_at?: string | null
           file_name?: string | null
           file_size?: number | null
           file_type?: string | null
@@ -34,7 +32,7 @@ export type Database = {
         }
         Update: {
           chat_id?: string
-          created_at?: string
+          created_at?: string | null
           file_name?: string | null
           file_size?: number | null
           file_type?: string | null
@@ -43,6 +41,20 @@ export type Database = {
           user_id?: string
         }
         Relationships: [
+          {
+            foreignKeyName: "chat_attachments_chat_id_fkey"
+            columns: ["chat_id"]
+            isOneToOne: false
+            referencedRelation: "chats"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "chat_attachments_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
           {
             foreignKeyName: "fk_chat"
             columns: ["chat_id"]
@@ -64,25 +76,25 @@ export type Database = {
           created_at: string | null
           id: string
           model: string | null
+          system_prompt: string | null
           title: string | null
           user_id: string
-          system_prompt: string | null
         }
         Insert: {
           created_at?: string | null
           id?: string
           model?: string | null
+          system_prompt?: string | null
           title?: string | null
           user_id: string
-          system_prompt?: string | null
         }
         Update: {
           created_at?: string | null
           id?: string
           model?: string | null
+          system_prompt?: string | null
           title?: string | null
           user_id?: string
-          system_prompt?: string | null
         }
         Relationships: [
           {
@@ -93,76 +105,6 @@ export type Database = {
             referencedColumns: ["id"]
           },
         ]
-      }
-      messages: {
-        Row: {
-          experimental_attachments: Attachment[]
-          chat_id: string
-          content: string
-          created_at: string | null
-          id: number
-          role: "system" | "user" | "assistant" | "data"
-          parent_message_id: number | null
-        }
-        Insert: {
-          experimental_attachments?: Attachment[]
-          chat_id: string
-          content: string
-          created_at?: string | null
-          id?: number
-          role: "system" | "user" | "assistant" | "data"
-        }
-        Update: {
-          experimental_attachments?: Attachment[]
-          chat_id?: string
-          content?: string
-          created_at?: string | null
-          id?: number
-          role?: "system" | "user" | "assistant" | "data"
-        }
-        Relationships: []
-      }
-      users: {
-        Row: {
-          anonymous: boolean | null
-          created_at: string | null
-          daily_message_count: number | null
-          daily_reset: string | null
-          display_name: string | null
-          email: string
-          id: string
-          message_count: number | null
-          preferred_model: string | null
-          premium: boolean | null
-          profile_image: string | null
-        }
-        Insert: {
-          anonymous?: boolean | null
-          created_at?: string | null
-          daily_message_count?: number | null
-          daily_reset?: string | null
-          display_name?: string | null
-          email: string
-          id: string
-          message_count?: number | null
-          preferred_model?: string | null
-          premium?: boolean | null
-          profile_image?: string | null
-        }
-        Update: {
-          anonymous?: boolean | null
-          created_at?: string | null
-          daily_message_count?: number | null
-          daily_reset?: string | null
-          display_name?: string | null
-          email?: string
-          id?: string
-          message_count?: number | null
-          preferred_model?: string | null
-          premium?: boolean | null
-          profile_image?: string | null
-        }
-        Relationships: []
       }
       feedback: {
         Row: {
@@ -193,6 +135,260 @@ export type Database = {
           },
         ]
       }
+      Logo: {
+        Row: {
+          color: string
+          filter: string | null
+          id: number
+          inserted_at: string | null
+          logoid: string
+          name: string
+          orderid: number
+          rotate: number
+          scale: number | null
+          strokewidth: number
+          updated_at: string | null
+          userid: string | null
+        }
+        Insert: {
+          color: string
+          filter?: string | null
+          id?: number
+          inserted_at?: string | null
+          logoid: string
+          name: string
+          orderid: number
+          rotate: number
+          scale?: number | null
+          strokewidth: number
+          updated_at?: string | null
+          userid?: string | null
+        }
+        Update: {
+          color?: string
+          filter?: string | null
+          id?: number
+          inserted_at?: string | null
+          logoid?: string
+          name?: string
+          orderid?: number
+          rotate?: number
+          scale?: number | null
+          strokewidth?: number
+          updated_at?: string | null
+          userid?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Logo_orderid_fkey"
+            columns: ["orderid"]
+            isOneToOne: false
+            referencedRelation: "Order"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "Logo_userid_fkey"
+            columns: ["userid"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      messages: {
+        Row: {
+          chat_id: string
+          content: string
+          created_at: string | null
+          experimental_attachments: Json | null
+          id: number
+          parent_message_id: number | null
+          reasoning_text: string | null
+          role: string
+          user_id: string | null
+        }
+        Insert: {
+          chat_id: string
+          content: string
+          created_at?: string | null
+          experimental_attachments?: Json | null
+          id?: number
+          parent_message_id?: number | null
+          reasoning_text?: string | null
+          role: string
+          user_id?: string | null
+        }
+        Update: {
+          chat_id?: string
+          content?: string
+          created_at?: string | null
+          experimental_attachments?: Json | null
+          id?: number
+          parent_message_id?: number | null
+          reasoning_text?: string | null
+          role?: string
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "messages_chat_id_fkey"
+            columns: ["chat_id"]
+            isOneToOne: false
+            referencedRelation: "chats"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      Order: {
+        Row: {
+          checkoutid: string | null
+          createdat: string | null
+          id: number
+          priceid: string | null
+          status: Database["public"]["Enums"]["orderstatus"] | null
+          updatedat: string | null
+          userid: string | null
+        }
+        Insert: {
+          checkoutid?: string | null
+          createdat?: string | null
+          id?: number
+          priceid?: string | null
+          status?: Database["public"]["Enums"]["orderstatus"] | null
+          updatedat?: string | null
+          userid?: string | null
+        }
+        Update: {
+          checkoutid?: string | null
+          createdat?: string | null
+          id?: number
+          priceid?: string | null
+          status?: Database["public"]["Enums"]["orderstatus"] | null
+          updatedat?: string | null
+          userid?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Order_userid_fkey"
+            columns: ["userid"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      purchases: {
+        Row: {
+          amount: number
+          created_at: string | null
+          id: number
+          status: string
+          stripe_payment_id: string
+          user_id: string
+        }
+        Insert: {
+          amount: number
+          created_at?: string | null
+          id?: number
+          status: string
+          stripe_payment_id: string
+          user_id: string
+        }
+        Update: {
+          amount?: number
+          created_at?: string | null
+          id?: number
+          status?: string
+          stripe_payment_id?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "purchases_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      usage_history: {
+        Row: {
+          created_at: string | null
+          id: number
+          message_count: number
+          period_end: string
+          period_start: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          message_count: number
+          period_end: string
+          period_start: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          message_count?: number
+          period_end?: string
+          period_start?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "usage_history_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      users: {
+        Row: {
+          anonymous: boolean
+          created_at: string
+          daily_message_count: number
+          daily_reset: string | null
+          display_name: string | null
+          email: string
+          id: string
+          message_count: number
+          preferred_model: string | null
+          premium: boolean
+          profile_image: string | null
+        }
+        Insert: {
+          anonymous?: boolean
+          created_at?: string
+          daily_message_count?: number
+          daily_reset?: string | null
+          display_name?: string | null
+          email: string
+          id: string
+          message_count?: number
+          preferred_model?: string | null
+          premium?: boolean
+          profile_image?: string | null
+        }
+        Update: {
+          anonymous?: boolean
+          created_at?: string
+          daily_message_count?: number
+          daily_reset?: string | null
+          display_name?: string | null
+          email?: string
+          id?: string
+          message_count?: number
+          preferred_model?: string | null
+          premium?: boolean
+          profile_image?: string | null
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
@@ -215,27 +411,29 @@ export type Database = {
   }
 }
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
         Row: infer R
       }
       ? R
@@ -243,20 +441,22 @@ export type Tables<
     : never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Insert: infer I
       }
       ? I
@@ -264,20 +464,22 @@ export type TablesInsert<
     : never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
         Update: infer U
       }
       ? U
@@ -285,21 +487,23 @@ export type TablesUpdate<
     : never
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
-> = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
     : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof PublicSchema["CompositeTypes"]
+    | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof Database
@@ -308,6 +512,14 @@ export type CompositeTypes<
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
+
+export const Constants = {
+  public: {
+    Enums: {
+      orderstatus: ["UNPAID", "PAID", "SHIPPED", "OUT", "CANCELLED", "PENDING"],
+    },
+  },
+} as const
