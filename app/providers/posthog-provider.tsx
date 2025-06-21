ddd4349d1eb7bfc4a613240a854103c5087d3ad6
AjanRaj <55903526+ajanraj@users.noreply.github.com>
2025-06-21 18:43:44 +0100

    fix: move PostHog initialization to window check for better compatibility

--- app/providers/posthog-provider.tsx
+++ app/providers/posthog-provider.tsx
@@ -4,12 +4,6 @@ import { PostHogProvider } from "posthog-js/react"
 import { useEffect } from "react"
 import { useUser } from "./user-provider"
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: "/ingest",
-    ui_host: 'https://eu.posthog.com'
-  })
-}
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return (
     <PostHogProvider client={posthog}>
--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,10 +1,12 @@
 import posthog from "posthog-js"
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  capture_pageview: "history_change",
-  capture_pageleave: true, // Enable pageleave capture
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
-  debug: process.env.NODE_ENV === "development",
-})
+if (typeof window !== "undefined") {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+        api_host: "/ingest",
+        ui_host: "https://eu.posthog.com",
+        capture_pageview: "history_change",
+        capture_pageleave: true, // Enable pageleave capture
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+        debug: process.env.NODE_ENV === "development",
+    })
+}
